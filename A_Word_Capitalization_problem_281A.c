fdcbc17b454cf3735f07d4536093b9ad0370fac1
Oscar Reizo <165621568+kepadahindia@users.noreply.github.com>
2026-01-31 09:23:44 +0700

    Add files via upload

---
+++ A_Word_Capitalization_problem_281A.c
@@ -0,0 +1,28 @@
+/* #include <stdio.h>
+
+int main() {
+    char word[105];
+    scanf("%s", word);
+
+    if (word[0] >= 'a' && word[0] <= 'z') {
+        word[0] = word[0] - 32;
+    }
+
+    printf("%s\n", word);
+    return 0;
+} */
+
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    char word[1005];   // AMAN untuk 1000 karakter
+
+    scanf("%s", word);
+
+    word[0] = toupper(word[0]);
+
+    printf("%s\n", word);
+
+    return 0;
+}
