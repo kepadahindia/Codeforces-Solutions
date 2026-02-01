f682585f111883e68deafe307a129959a4eee2f9
Oscar Reizo <165621568+kepadahindia@users.noreply.github.com>
2026-02-01 16:18:33 +0700

    Update A_Stones_on_the_Table_problem_226A.c

--- A_Stones_on_the_Table_problem_226A.c
+++ A_Stones_on_the_Table_problem_226A.c
@@ -1,17 +1,17 @@
     #include <stdio.h>
 
 int main() {
-    int n;
-    char s[51];
+    int n; // Jumlah batu di meja   
+    char s[51]; // Urutan warna batu (string). Maksimal batu adalah 50, Tambahan 1 untuk karakter penutup string '\0'
 
-    scanf("%d", &n);
-    scanf("%s", s);
+    scanf("%d", &n); // Satu bilangan bulat, Disimpan ke variabel n
+    scanf("%s", s); // Membaca string warna batu, Disimpan ke array s
 
-    int count = 0;
+    int count = 0; // Menyimpan jumlah batu yang harus dihapus. Awalnya belum ada batu yang diambil
 
-    for (int i = 1; i < n; i++) {
-        if (s[i] == s[i - 1]) {
-            count++;
+    for (int i = 1; i < n; i++) { // Iterasi dimulai dari i = 1 hingga i < n.  
+        if (s[i] == s[i - 1]) { // Misal i = 1, maka s[1] == s[0], membandingkan batu ke-2 dengan batu ke-1. INI KONSEP ARRAY.
+            count++; // Jika warna batu saat ini sama dengan warna batu sebelumnya, maka kita perlu menghapus batu ini. Dan count batu yang terhapus akan bertambah 1.
         }
     }
 
